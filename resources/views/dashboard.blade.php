73fb09646a1e9879adec3637af679dedc273375e
NicholasRudy2172020 <91111940+NicholasRudy2172020@users.noreply.github.com>
2023-06-25 13:59:48 +0700

    Add files via upload

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,14 @@
 	<div class="container-fluid">
 		<div class="row mb-2">
 			<div class="col-sm-6">
-				<h1 class="m-0 text-dark">Dashboard</h1>
+                @auth
+                    @if (Auth::user()->hasRole('prodi'))
+                        <h1 class="m-0 text-dark">Dashboard Prodi</h1>
+                    @elseif (Auth::user()->hasRole('mahasiswa'))
+                        <h1 class="m-0 text-dark">Dashboard Mahasiswa</h1>
+                    @endif
+                @endauth
+
 			</div><!-- /.col -->
 			<div class="col-sm-6">
 				<ol class="breadcrumb float-sm-right">
@@ -21,7 +28,17 @@
 <!-- Main content -->
 <div class="content">
 	<div class="container-fluid">
-		
+        <ul>
+            @auth
+                @if (Auth::user()->hasRole('prodi'))
+                    <li><a href="{{ route('prodi.dkbs') }}">DKBS Mahasiswa</a></li>
+                    <li><a href="{{ route('prodi.tawarmk') }}">Penawaran Mata Kuliah</a></li>
+                @elseif (Auth::user()->hasRole('mahasiswa'))
+                    <li><a href="{{ route('dkbs') }}">DKBS</a></li>
+                    <li><a href="{{ route('mk_tawar') }}">Perwalian</a></li>
+                @endif
+            @endauth
+        </ul>
 	</div><!-- /.container-fluid -->
 </div>
 <!-- /.content -->
