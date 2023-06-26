258028157111614401472cc5f51e5e6d5a942acf
frangkyyy <126849323+frangkyyy@users.noreply.github.com>
2023-06-26 19:50:18 +0700

    Update dkbsController.php

--- app/Http/Controllers/dkbsController.php
+++ app/Http/Controllers/dkbsController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class dkbsController extends Controller
 {
-    //
+    public static function index()
+    {
+        $periods = [
+            'Reguler Ganjil 2021/2022',
+            'Reguler Genap 2021/2022',
+            'Antara Genap 2021/2022',
+            'Reguler Ganjil 2022/2023',
+            'Reguler Genap 2022/2023',
+        ];
+
+        return view('education.periods')->with('periods', $periods);
+    }
 }
+
