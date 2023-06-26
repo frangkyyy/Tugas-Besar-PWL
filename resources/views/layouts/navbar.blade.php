8d893b01b8e2d4aef0f38f3f544e6404b2cb40ec
Rizky Jeremia Simanjutak <90759795+RizkySimanjuntak@users.noreply.github.com>
2023-06-26 22:37:06 +0700

    Update navbar.blade.php

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -10,7 +10,7 @@
         </li>
     </ul>
 
-     <!-- SEARCH FORM -->
+<!--      <!-- SEARCH FORM -->
     <form class="form-inline ml-3">
         <div class="input-group input-group-sm">
             <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
@@ -20,7 +20,7 @@
                 </button>
             </div>
         </div>
-    </form>
+    </form> -->
 
      <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
